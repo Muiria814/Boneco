c770d7ffb8a271f7ac95f137299bb08f13d8f134
Eleuterio Raul <emuiria@gmail.com>
2025-12-23 11:43:16 +0200

    Update script.js

--- script.js
+++ script.js
@@ -164,8 +164,8 @@ withdrawBtn.addEventListener("click", async () => {
 
   withdrawMessage.style.color = "green";
   withdrawMessage.textContent = "Levantamento solicitado com sucesso ✅";
-  dogeAddressInput.value = "";
-    }
+ 
+      dogeAddressInput.value = "";
    } else {
       withdrawMessage.style.color = "red";
       withdrawMessage.textContent = data.message || "Erro no levantamento.";
