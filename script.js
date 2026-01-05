a2267ae901fefcb6f7572ebdb09d3393f4e6a2b6
Eleuterio Raul <emuiria@gmail.com>
2026-01-05 20:53:11 +0200

    Update minimum withdrawal amount to 0.0001

--- script.js
+++ script.js
@@ -42,7 +42,7 @@ const withdrawMessage = document.getElementById("withdraw-message");
 
 // ====== CONFIGURAÇÕES ======
 const DOGE_POR_PASSOS = 1000;
-const MIN_SAQUE = 1;
+const MIN_SAQUE = 0.0001;
 const COOLDOWN_CONVERT = 5000; // 5 segundos para teste
 
 // ====== ESTADO ======
